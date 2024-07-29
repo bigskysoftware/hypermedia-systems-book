4a65b3181e5e98ba0b2e7e98ef60246cc398750a
Deniz Akşimşek <deniz@denizaksimsek.com>
2024-07-29 03:35:49 +0300

    no more type errors in build_web

--- www/build_web.ts
+++ www/build_web.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno run -A --unstable
 
-import { Element, Text } from "https://codeberg.org/dz4k/muteferrika/raw/commit/3c0e7a3551fda9b8808b312e3e88750c0fd3e3ed/lib/deps/deno-dom.ts";
-import { build, Book, Frontmatter, Copyright, Dedication, Foreword, Part, Introduction, Chapter, write, TableOfContents, LandingPage, Division } from "https://codeberg.org/dz4k/muteferrika/raw/commit/3c0e7a3551fda9b8808b312e3e88750c0fd3e3ed/lib/muteferrika.ts";
+import { Element, Text } from "https://codeberg.org/dz4k/muteferrika/raw/commit/01fd5d0aadb6955c8f5f604312fd32f8db3d6bfc/lib/deps/deno-dom.ts";
+import { build, Book, Frontmatter, Copyright, Dedication, Foreword, Part, Introduction, Chapter, write, TableOfContents, LandingPage, Division } from "https://codeberg.org/dz4k/muteferrika/raw/commit/01fd5d0aadb6955c8f5f604312fd32f8db3d6bfc/lib/muteferrika.ts";
 import { copySync } from "https://deno.land/std@0.157.0/fs/copy.ts";
 
 
@@ -26,7 +26,7 @@ const compile = async (path: string) => {
     title,
     url,
     // add ids to headings
-    process(this: Division) {
+    process(this: Division): undefined {
       const ids = new Map<string, number>();
       this.dom.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((node) => {
         const heading = node as Element
@@ -52,12 +52,11 @@ const compile = async (path: string) => {
           text.data = text.data.replace("℄", "");
         }
       })
-      return this.compiledContent
     }
-  } as Partial<Division>;
+  };
 }
 
-const HypermediaSystems = new Book("Hypermedia Systems",
+const HypermediaSystems = new Book("Hypermedia Systems").with(
   {
     lang: "en",
     htmlHead: `
@@ -76,43 +75,43 @@ const HypermediaSystems = new Book("Hypermedia Systems",
     </footer>
     `
   },
-  new LandingPage("Hypermedia Systems", { url: '/', content: await Deno.readTextFile("www/cover.html") }),
-  new Frontmatter(
-    new Copyright(await compile("-1-copy-ack.typ"), "Copyright & Acknowledgments"),
-    new Dedication(await compile("-2-dedication.typ"), "Dedication"),
-    new Foreword(await compile("-3-foreword.typ"), "Foreword"),
-    new TableOfContents("Contents", {
+  new LandingPage("Hypermedia Systems").with({ url: '/', content: await Deno.readTextFile("www/cover.html") }),
+  new Frontmatter().with(
+    new Copyright("Copyright & Acknowledgments").with(await compile("-1-copy-ack.typ")),
+    new Dedication("Dedication").with(await compile("-2-dedication.typ")),
+    new Foreword("Foreword").with(await compile("-3-foreword.typ")),
+    new TableOfContents("Contents").with({
       url: '/book/contents/',
       file: "www/tocheader.html",
       compile: it => it
     }),
   ),
-  new Part("Hypermedia Concepts",
+  new Part("Hypermedia Concepts").with(
     { url: '/part/hypermedia-concepts/' },
-    new Introduction(await compile("ch00-introduction.typ")),
-    new Chapter(await compile("ch01-hypermedia-a-reintroduction.typ")),
-    new Chapter(await compile("ch02-components-of-a-hypermedia-system.typ")),
-    new Chapter(await compile("ch03-a-web-1-0-application.typ")),
+    new Introduction().with(await compile("ch00-introduction.typ")),
+    new Chapter().with(await compile("ch01-hypermedia-a-reintroduction.typ")),
+    new Chapter().with(await compile("ch02-components-of-a-hypermedia-system.typ")),
+    new Chapter().with(await compile("ch03-a-web-1-0-application.typ")),
   ),
-  new Part("Hypermedia-Driven Web Applications With Htmx",
+  new Part("Hypermedia-Driven Web Applications With Htmx").with(
     { url: '/part/htmx/' },
-    new Chapter(await compile("ch04-extending-html-as-hypermedia.typ")),
-    new Chapter(await compile("ch05-htmx-patterns.typ")),
-    new Chapter(await compile("ch06-more-htmx-patterns.typ")),
-    new Chapter(await compile("ch07-a-dynamic-archive-ui.typ")),
-    new Chapter(await compile("ch08-tricks-of-the-htmx-masters.typ")),
-    new Chapter(await compile("ch09-client-side-scripting.typ")),
-    new Chapter(await compile("ch10-json-data-apis.typ")),
+    new Chapter().with(await compile("ch04-extending-html-as-hypermedia.typ")),
+    new Chapter().with(await compile("ch05-htmx-patterns.typ")),
+    new Chapter().with(await compile("ch06-more-htmx-patterns.typ")),
+    new Chapter().with(await compile("ch07-a-dynamic-archive-ui.typ")),
+    new Chapter().with(await compile("ch08-tricks-of-the-htmx-masters.typ")),
+    new Chapter().with(await compile("ch09-client-side-scripting.typ")),
+    new Chapter().with(await compile("ch10-json-data-apis.typ")),
   ),
-  new Part("Bringing Hypermedia To Mobile",
+  new Part("Bringing Hypermedia To Mobile").with(
     { url: '/part/hyperview/' },
-    new Chapter(await compile("ch11-hyperview-a-mobile-hypermedia.typ")),
-    new Chapter(await compile("ch12-building-a-contacts-app-with-hyperview.typ")),
-    new Chapter(await compile("ch13-extending-the-hyperview-client.typ")),
+    new Chapter().with(await compile("ch11-hyperview-a-mobile-hypermedia.typ")),
+    new Chapter().with(await compile("ch12-building-a-contacts-app-with-hyperview.typ")),
+    new Chapter().with(await compile("ch13-extending-the-hyperview-client.typ")),
   ),
-  new Part("Conclusion",
+  new Part("Conclusion").with(
     { url: '/part/conclusion/' },
-    new Chapter(await compile("ch14-conclusion.typ")),
+    new Chapter().with(await compile("ch14-conclusion.typ")),
   ),
 );
 
