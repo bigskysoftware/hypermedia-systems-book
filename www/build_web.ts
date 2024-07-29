cb4edd13f17f5a6cdfc0ea15bdc005914c7259dc
Deniz Akşimşek <deniz@denizaksimsek.com>
2024-07-29 16:50:10 +0300

    parallelize html build by upgrading muteferrika

--- www/build_web.ts
+++ www/build_web.ts
@@ -1,32 +1,33 @@
 #!/usr/bin/env -S deno run -A --unstable
 
-import { Element, Text } from "https://codeberg.org/dz4k/muteferrika/raw/commit/01fd5d0aadb6955c8f5f604312fd32f8db3d6bfc/lib/deps/deno-dom.ts";
-import { build, Book, Frontmatter, Copyright, Dedication, Foreword, Part, Introduction, Chapter, write, TableOfContents, LandingPage, Division } from "https://codeberg.org/dz4k/muteferrika/raw/commit/01fd5d0aadb6955c8f5f604312fd32f8db3d6bfc/lib/muteferrika.ts";
+import { Element, Text } from "https://codeberg.org/dz4k/muteferrika/raw/commit/e7ccf8d28b309acbd9ef4ec0b24d6bae12c2c814/lib/deps/deno-dom.ts";
+import { build, Book, Frontmatter, Copyright, Dedication, Foreword, Part, Introduction, Chapter, write, TableOfContents, LandingPage, Division } from "https://codeberg.org/dz4k/muteferrika/raw/commit/e7ccf8d28b309acbd9ef4ec0b24d6bae12c2c814/lib/muteferrika.ts";
 import { copySync } from "https://deno.land/std@0.157.0/fs/copy.ts";
 
 
-const compile = async (path: string) => {
-  const pandoc = new Deno.Command("pandoc", {
-    args: ["-f", "typst", "-t", "html", "--", path],
-  });
-  const pandocOutput = await pandoc.output()
-  if (!pandocOutput.success) {
-    console.error("pandoc:", new TextDecoder().decode(pandocOutput.stderr))
-    Deno.exit(pandocOutput.code)
-  }
-  const compiled = new TextDecoder().decode(pandocOutput.stdout);
-  const headingsUpleveled = compiled.replace(/<(\/?)h(\d)/g, (_, slash, level) => `<${slash}h${+level - 1}`);
-  const title = headingsUpleveled.match(/<h1>(.*)<\/h1>/)?.[1];
-  const h1Removed = headingsUpleveled.replace(/<h1>.*<\/h1>/, "");
-  const url = path
-    .replace(/\.typ$/, "/")
-    .replace(/ch\d\d-|-\d-/, "/");
+const compile = (path: string) => {
   return {
-    compiledContent: h1Removed,
-    title,
-    url,
-    // add ids to headings
-    process(this: Division): undefined {
+    async compile(this: Division) {
+      const pandoc = new Deno.Command("pandoc", {
+        args: ["-f", "typst", "-t", "html", "--", path],
+      });
+      const pandocOutput = await pandoc.output()
+      if (!pandocOutput.success) {
+        console.error("pandoc:", new TextDecoder().decode(pandocOutput.stderr))
+        Deno.exit(pandocOutput.code)
+      }
+      const compiled = new TextDecoder().decode(pandocOutput.stdout);
+      const headingsUpleveled = compiled.replace(/<(\/?)h(\d)/g, (_, slash, level) => `<${slash}h${+level - 1}`);
+      this.title = headingsUpleveled.match(/<h1>(.*)<\/h1>/)?.[1];
+      const h1Removed = headingsUpleveled.replace(/<h1>.*<\/h1>/, "");
+      return h1Removed
+    },
+    url: path
+      .replace(/\.typ$/, "/")
+      .replace(/ch\d\d-|-\d-/, "/"),
+    // deno-lint-ignore require-await
+    async process(this: Division) {
+      // add ids to headings
       const ids = new Map<string, number>();
       this.dom.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((node) => {
         const heading = node as Element
@@ -77,41 +78,41 @@ const HypermediaSystems = new Book("Hypermedia Systems").with(
   },
   new LandingPage("Hypermedia Systems").with({ url: '/', content: await Deno.readTextFile("www/cover.html") }),
   new Frontmatter().with(
-    new Copyright("Copyright & Acknowledgments").with(await compile("-1-copy-ack.typ")),
-    new Dedication("Dedication").with(await compile("-2-dedication.typ")),
-    new Foreword("Foreword").with(await compile("-3-foreword.typ")),
+    new Copyright("Copyright & Acknowledgments").with(compile("-1-copy-ack.typ")),
+    new Dedication("Dedication").with(compile("-2-dedication.typ")),
+    new Foreword("Foreword").with(compile("-3-foreword.typ")),
     new TableOfContents("Contents").with({
       url: '/book/contents/',
       file: "www/tocheader.html",
-      compile: it => it
+      compile: it => Promise.resolve(it)
     }),
   ),
   new Part("Hypermedia Concepts").with(
     { url: '/part/hypermedia-concepts/' },
-    new Introduction().with(await compile("ch00-introduction.typ")),
-    new Chapter().with(await compile("ch01-hypermedia-a-reintroduction.typ")),
-    new Chapter().with(await compile("ch02-components-of-a-hypermedia-system.typ")),
-    new Chapter().with(await compile("ch03-a-web-1-0-application.typ")),
+    new Introduction().with(compile("ch00-introduction.typ")),
+    new Chapter().with(compile("ch01-hypermedia-a-reintroduction.typ")),
+    new Chapter().with(compile("ch02-components-of-a-hypermedia-system.typ")),
+    new Chapter().with(compile("ch03-a-web-1-0-application.typ")),
   ),
   new Part("Hypermedia-Driven Web Applications With Htmx").with(
     { url: '/part/htmx/' },
-    new Chapter().with(await compile("ch04-extending-html-as-hypermedia.typ")),
-    new Chapter().with(await compile("ch05-htmx-patterns.typ")),
-    new Chapter().with(await compile("ch06-more-htmx-patterns.typ")),
-    new Chapter().with(await compile("ch07-a-dynamic-archive-ui.typ")),
-    new Chapter().with(await compile("ch08-tricks-of-the-htmx-masters.typ")),
-    new Chapter().with(await compile("ch09-client-side-scripting.typ")),
-    new Chapter().with(await compile("ch10-json-data-apis.typ")),
+    new Chapter().with(compile("ch04-extending-html-as-hypermedia.typ")),
+    new Chapter().with(compile("ch05-htmx-patterns.typ")),
+    new Chapter().with(compile("ch06-more-htmx-patterns.typ")),
+    new Chapter().with(compile("ch07-a-dynamic-archive-ui.typ")),
+    new Chapter().with(compile("ch08-tricks-of-the-htmx-masters.typ")),
+    new Chapter().with(compile("ch09-client-side-scripting.typ")),
+    new Chapter().with(compile("ch10-json-data-apis.typ")),
   ),
   new Part("Bringing Hypermedia To Mobile").with(
     { url: '/part/hyperview/' },
-    new Chapter().with(await compile("ch11-hyperview-a-mobile-hypermedia.typ")),
-    new Chapter().with(await compile("ch12-building-a-contacts-app-with-hyperview.typ")),
-    new Chapter().with(await compile("ch13-extending-the-hyperview-client.typ")),
+    new Chapter().with(compile("ch11-hyperview-a-mobile-hypermedia.typ")),
+    new Chapter().with(compile("ch12-building-a-contacts-app-with-hyperview.typ")),
+    new Chapter().with(compile("ch13-extending-the-hyperview-client.typ")),
   ),
   new Part("Conclusion").with(
     { url: '/part/conclusion/' },
-    new Chapter().with(await compile("ch14-conclusion.typ")),
+    new Chapter().with(compile("ch14-conclusion.typ")),
   ),
 );
 
